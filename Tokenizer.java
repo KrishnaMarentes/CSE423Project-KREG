dfb9c780b634e036809ead1214267d4c39dbcc2e
Geoffrey Knox <geoffreyknox56@gmail.com>
2020-01-27 12:03:55 -0700

    Added pointer notation to supported operators

--- Tokenizer.java
+++ Tokenizer.java
@@ -37,43 +37,48 @@ public class Tokenizer
 
         ArrayList<String> tokens = TokenizeFile(filename);
 
-        for(String s : tokens) {
-            System.out.println(s);
+        if(tokens != null) {
+            for(String s : tokens) {
+                System.out.println(s);
+            }
         }
 
     }
 
-    //tokenize the file
+    //tokenize the file, return null if file can't be opened or error reading file
     public static ArrayList<String> TokenizeFile(String file)
     {
         ArrayList<String> tokens = new ArrayList<>();
         String line;
-        String source = "";
+        StringBuilder source = new StringBuilder();
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             //probably the least efficient way of doing this.
             //should work as long as the file is less than 2GB ¯\_(ツ)_/¯
             while((line = br.readLine()) != null) {
-                source += line + '\n';
+                source.append(line).append('\n');
             }
-            source = remove_comments(source);
+            br.close();
         } catch (FileNotFoundException e) {
             System.out.println(
             "Unable to open file '" +
             file + "'");
+            return null;
         } catch (IOException e) {
             System.out.println(
             "Error reading file '" +
             file + "'");
+            return null;
         }
 
-        Pattern pattern = Pattern.compile("#.+>|>>=?|<<=?|\\|\\||&&|[|\\^&%<>!\\-+*\\/=]?=|--|\\+\\+|\".+\"|[\\w]+|[\\W]|.+");
+        source = remove_comments(source);
+
+        Pattern pattern = Pattern.compile("#.+>|->|>>=?|<<=?|\\|\\||&&|[|^&%<>!\\-+*/=]?=|--|\\+\\+|\".+\"|[\\w]+|[\\W]|.+");
         Matcher matcher = pattern.matcher(source);
 
         while (matcher.find()) {
             String match = matcher.group(0);
-            //This conditional might be necessary to remove later. Might generate bugs.
             match = match.replaceAll("\\s|\n|\t", "");
             if(match.length() > 0) {
                 tokens.add(matcher.group(0));
@@ -86,9 +91,9 @@ public class Tokenizer
     /*
         Removes comments from file and places uncommented source code into a String.
     */
-    private static String remove_comments(String source)
+    private static StringBuilder remove_comments(StringBuilder source)
     {
-        return(source.replaceAll("\\/\\/.+\\n|(?s)\\/\\*.*?\\*\\/", ""));
+        return(new StringBuilder(source.toString().replaceAll("//.+\\n|(?s)/\\*.*?\\*/", "")));
     }
 
     /*
