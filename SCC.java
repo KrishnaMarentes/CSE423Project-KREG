fc5263c889005f7f24e4037e7a8be3bfe2eb1f2d
Krishna Marentes <krishna.marentes@student.nmt.edu>
2020-03-29 01:07:00 -0600

    read/write IR parts done and working

--- SCC.java
+++ SCC.java
@@ -1,16 +1,17 @@
 import org.antlr.v4.runtime.*;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class SCC {
     public static void main(String[] args) {
         String filename = null;
+        String write_filename = null;
+        String read_filename = null;
+
         CharStream src_code = null;
         char opt;
         int i = 0;
@@ -18,28 +19,61 @@ public class SCC {
         boolean print_pt = false;
         boolean print_ast = false;
         boolean print_ir = false;
+        boolean writefile = false;
+        boolean readfile = false;
+        boolean print_st = false;
         boolean save_output = false;
+
+        Scanner in = new Scanner(System.in);
 
         if(args.length > 0) {
             try {
-                /* Command line arguments for file name, parse tree, IR, or token list */
+                /* Command line arguments for file name */
                 if (args[0].charAt(0) == '-') {
                     for (i = 1; i < args[0].length(); i++) {
                         opt = args[0].charAt(i);
                         switch (opt) {
-                            case 'a':
+                            case 'a': /* print ast */
                                 print_ast = true;
                                 break;
-                            case 't':
+                            case 't': /* print tokens */
                                 print_tks = true;
                                 break;
-                            case 'p':
+                            case 'p': /* print parse tree */
                                 print_pt = true;
                                 break;
-                            case 'i':
+                            case 'i': /* print intermediate representation */
                                 print_ir = true;
                                 break;
-                            case 's':
+                            case 'w': /* write out IR to a file with a specified name */
+                                /* output filename will look: write_filename.out */
+                                writefile = true;
+                                System.out.println("Enter the name of the file to write out to: ");
+                                write_filename = in.nextLine();
+                                print_ir = true;
+                                break;
+                            case 'r': /* read in an IR specified instead of a source file */
+                                /* will read in any type of file and then output it to the terminal for now */
+                                /* ex of read_filename: src/tests/ex9.c or src/tests/ir_example.ir or src/tests/ir_example2.ast */
+                                readfile = true; /* might need for later */
+                                System.out.println("Enter the name of the IR file to read in: ");
+                                read_filename = in.nextLine();
+                                try {
+                                    File input = new File(read_filename);
+                                    Scanner readF = new Scanner(input);
+                                    while (readF.hasNextLine()) {
+                                        String IRdata = readF.nextLine();
+                                        System.out.println(IRdata);
+                                    }
+                                    readF.close();
+                                } catch (FileNotFoundException e) {
+                                    System.out.println("Error: file " + read_filename + " not found.");
+                                }
+                                break;
+                            case 's': /* print symbol table */
+                                print_st = true;
+                                break;
+                            case 'o': /* replacing 's' with 'o' */
                                 save_output = true;
                                 break;
                             default:
@@ -50,7 +84,6 @@ public class SCC {
                         }
                     }
                 }
-
                 filename = args[args.length-1];
                 src_code = CharStreams.fromFileName(filename);
             } catch (IOException e) {
@@ -84,7 +117,6 @@ public class SCC {
             }
         }
 
-
         //TODO: Allow to just save to file without having to also print to terminal
         if (print_tks) {
             System.out.println("printing tokens...");
@@ -109,8 +141,8 @@ public class SCC {
         }
         if (print_ir) {
             System.out.println("printing IR...");
-            if (save_output)
-                printIR(tree, parser.getRuleNames(), filename);
+            if (writefile)
+                printIR(tree, parser.getRuleNames(), write_filename);
             else
                 printIR(tree, parser.getRuleNames(), null);
         }
@@ -124,23 +156,25 @@ public class SCC {
         // TODO: Add error messages when invalid declarations are made
     }
 
-    private static void printIR(RuleContext rc, String[] ruleNames, String filename) {
+    private static void printIR(RuleContext rc, String[] ruleNames, String write_filename) {
         List<String> ruleNamesList = Arrays.asList(ruleNames);
         ASTNode an = TreeUtils.generateAST(rc, ruleNamesList);
 
         String ir = generateIR(an);
-        System.out.println(ir);
-        if (filename != null) {
+        String write_file = "src/tests/" + write_filename; /* redirecting to appropriate folder */
+
+        if (write_filename != null) {
             try {
-                FileWriter f = new FileWriter(filename + ".out", true);
+                FileWriter f = new FileWriter(write_file + ".out");
                 BufferedWriter b = new BufferedWriter(f);
                 b.write(ir + "\n\n");
                 b.close();
                 f.close();
             } catch (IOException e) {
-                System.out.println("An error occurred when attempting to save the output to a file");
+                System.out.println("An error occurred when attempting to save the output with the specified filename");
             }
-        }
+        } else
+            System.out.println(ir);
     }
 
     private static void printAST(RuleContext rc, String[] ruleNames, String filename) {
@@ -207,12 +241,15 @@ public class SCC {
 
     private static void usage() {
         System.out.println("usage: java [OPTS] FILENAME");
-        System.out.println("OPTS: [t, p, a, i, s]");
+        System.out.println("OPTS: [t, p, a, i, w, r, s, o]");
         System.out.println("t: Print the tokens");
         System.out.println("p: Print the parse tree");
         System.out.println("a: Print the abstract syntax tree");
         System.out.println("i: Print the intermediate representation");
-        System.out.println("s: Save all printing to a file named 'FILENAME.out'");
+        System.out.println("w: Write IR out to a file with a specified name");
+        System.out.println("r: Read in an IR specified instead of a source file");
+        System.out.println("s: Print the symbol table");
+        System.out.println("o: Save all printing to a file named 'FILENAME.out'");
         System.out.println("FILENAME: file path");
     }
 }
