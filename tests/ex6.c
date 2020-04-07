c9d1740170d5ec33bc9741758ba3dce390240939
Geoffrey Knox <30853504+quazar22@users.noreply.github.com>
2020-04-06 22:22:48 -0600

    removing switch usage

--- tests/ex6.c
+++ tests/ex6.c
@@ -3,27 +3,6 @@ int main()
     char grade = 'B';
     int num, curvedgrade;
 
-    switch(grade)
-    {
-        case 'A':
-            num = 1;
-            break;
-        case 'B':
-            num = 2;
-            break;
-        case 'C':
-            num = 3;
-            break;
-        case 'D':
-            num = 4;
-            break;
-        case 'F':
-            num = 5;
-            break;
-        default:
-            num = 6;
-    }
-
     if(grade == 'A') {
         num = 90;
     } else if(grade == 'B') {
